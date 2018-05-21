95356ffde6488d19c21e7d58373199fc3df2a0e7
heitorrapela <hrm@cin.ufpe.br>
2018-05-21 12:52:04 -0300

    Make sublime text default in rosed roscpp filename

--- install_sublime.sh
+++ install_sublime.sh
@@ -1,4 +1,6 @@
 sudo add-apt-repository ppa:webupd8team/sublime-text-3;
 sudo apt-get update;
-sudo apt-get install sublime-text-installer;
+sudo apt-get install -y sublime-text-installer;
 sudo ln -s /usr/lib/sublime-text-3/sublime_text /usr/local/bin/sublime;
+echo "export EDITOR='subl -w'" >> ~/.bashrc
+source ~/.bashrc

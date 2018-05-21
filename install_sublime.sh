12660f6980a647bf0a56d4315bcb48ba8c417100
heitorrapela <hrm@cin.ufpe.br>
2018-05-21 11:23:22 -0300

    Install sublime text inside Ubuntu 14.04LTS

---
+++ install_sublime.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:webupd8team/sublime-text-3;
+sudo apt-get update;
+sudo apt-get install sublime-text-installer;
+sudo ln -s /usr/lib/sublime-text-3/sublime_text /usr/local/bin/sublime;
